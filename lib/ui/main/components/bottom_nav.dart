257c594df89b5b0ad77e36977e050ee58e401420
rach <bbazzuraa@gmail.com>
2026-02-20 16:26:31 +0700

    added shadow to navbar

--- lib/ui/main/components/bottom_nav.dart
+++ lib/ui/main/components/bottom_nav.dart
@@ -5,7 +5,6 @@ class BottomNav extends StatelessWidget {
   final int selectedIndex;
   final ValueChanged<int> onItemTapped;
 
-
   const BottomNav({
     super.key,
     required this.selectedIndex,
@@ -14,61 +13,75 @@ class BottomNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const Color activeColor = Color(0xFF2196F3);
+    const Color inactiveColor = Color(0xFF6B7280);
 
-    const Color activeColor = Color(0xFF2196F3); // Blue when active
-    const Color inactiveColor = Color(0xFF6B7280); // Grey when inactive
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.08),
+            blurRadius: 10,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/home.svg',
+              color: selectedIndex == 0 ? activeColor : inactiveColor,
+              width: 35,
+              height: 35,
+            ),
+            label: 'Home',
+          ),
 
-    return BottomNavigationBar(
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/icons/home.svg',
-            color: selectedIndex == 0 ? activeColor : inactiveColor,
-            width: 35,
-            height: 35,
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/chat.svg',
+              color: selectedIndex == 1 ? activeColor : inactiveColor,
+              width: 35,
+              height: 35,
+            ),
+            label: 'Chat',
           ),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/icons/chat.svg',
-            color: selectedIndex == 1 ? activeColor : inactiveColor,
-            width: 35,
-            height: 35,
-          ),
-          label: 'Chat',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/icons/file-text.svg',
-            color: selectedIndex == 2 ? activeColor : inactiveColor,
-            width: 35,
-            height: 35,
-          ),
-          label: 'Articles',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/icons/user-circle.svg',
-            color: selectedIndex == 3 ? activeColor : inactiveColor,
-            width: 35,
-            height: 35,
-          ),
-          label: 'Profile',
-        ),
-      ],
 
-      currentIndex: selectedIndex,
-      selectedItemColor: activeColor,
-      unselectedItemColor: inactiveColor,
-      backgroundColor: Colors.white,
-      type: BottomNavigationBarType.fixed,
-      onTap: onItemTapped,
-      elevation: 8,
-      selectedFontSize: 12,
-      unselectedFontSize: 12,
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/file-text.svg',
+              color: selectedIndex == 2 ? activeColor : inactiveColor,
+              width: 35,
+              height: 35,
+            ),
+            label: 'Articles',
+          ),
+
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/user-circle.svg',
+              color: selectedIndex == 3 ? activeColor : inactiveColor,
+              width: 35,
+              height: 35,
+            ),
+            label: 'Profile',
+          ),
+        ],
+
+        currentIndex: selectedIndex,
+        selectedItemColor: activeColor,
+        unselectedItemColor: inactiveColor,
+        backgroundColor: Colors.transparent,
+        type: BottomNavigationBarType.fixed,
+        onTap: onItemTapped,
+        elevation: 0,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+      ),
     );
   }
 }
